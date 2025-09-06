128321f572c7366c3af51890d794da00044694bf
Michael Vargas S. <michaelvargas47@hotmail.com>
2025-09-05 18:45:08 -0600

    Avance evaluacion:Llega hasta la pantalla de Evaluar

--- CUC_Evaluacion_Desemp/Controllers/EvaluacionController.cs
+++ CUC_Evaluacion_Desemp/Controllers/EvaluacionController.cs
@@ -285,6 +285,123 @@ namespace CUC_Evaluacion_Desemp.Controllers
             }
         }//fin
 
+        [HttpPost]
+        public ActionResult SeleccionarSubalternoParaEvaluar(string cedulaSeleccionada)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(cedulaSeleccionada))
+                {
+                    TempData["MensajeError"] = "Debe seleccionar un subalterno.";
+                    return RedirectToAction("SeleccionarSubalternoParaEvaluar");
+                }
+
+                return RedirectToAction("ConglomeradosPorFuncAParaEvaluar", new { cedulaSeleccionada = cedulaSeleccionada });
+
+            }
+            catch (Exception)
+            {
+                TempData["MensajeError"] = "Error al seleccionar Funcionario.";
+                return RedirectToAction(nameof(SeleccionarSubalterno));
+            }
+        }
+
+        public ActionResult ConglomeradosPorFuncAParaEvaluar(string cedulaSeleccionada)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(cedulaSeleccionada))
+                {
+                    TempData["MensajeError"] = "Debe seleccionar un Conglomerado para el funcionario a evaluar.";
+                    return RedirectToAction("SeleccionarSubalternoParaEvaluar");
+                }
+
+                ViewBag.cedulaSeleccionada = cedulaSeleccionada;
+
+                var listaConglomerados = _servicioMantenimientos.Funcionario.ConglomeradosPorFunc(cedulaSeleccionada);
+                return View(listaConglomerados);
+            }
+            catch (Exception)
+            {
+                TempData["MensajeError"] = "Error al obtener la lista.";
+                return RedirectToAction(nameof(SeleccionarSubalternoParaEvaluar));
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult SelecPeriodoPorFunParaEvaluar(FormCollection coleccion)
+        {
+            try
+            {
+                string cedulaSeleccionada = coleccion["cedulaSeleccionada"];
+                string idConglomerado = coleccion["idConglomerado"];
+
+                ViewBag.cedulaSeleccionada = cedulaSeleccionada;
+                ViewBag.idConglomerado = idConglomerado;
+
+                var periodos = _servicioMantenimientos.Periodos.ListarPeriodosAnioActual();
+                return View(periodos);
+            }
+            catch (Exception)
+            {
+                TempData["MensajeError"] = "Error al obtener la lista.";
+                return RedirectToAction(nameof(SeleccionarSubalternoParaEvaluar));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EvaluarSubAlterno(FormCollection coleccion)
+        {
+            try
+            {
+                string cedulaSeleccionada = coleccion["cedulaSeleccionada"];
+                int idConglomerado = Convert.ToInt32(coleccion["idConglomerado"]);
+                string idPeriodo = coleccion["idPeriodo"];
+
+                if (string.IsNullOrEmpty(cedulaSeleccionada) || string.IsNullOrEmpty(coleccion["idConglomerado"]))
+                {
+                    TempData["MensajeError"] = "Debe seleccionar un funcionario y un conglomerado.";
+                    return RedirectToAction("SeleccionarSubalterno");
+                }
+
+                //obtenemos el funcionario
+                var subalterno = _servicioMantenimientos.Funcionario.ConsultarFuncionarioID(cedulaSeleccionada);
+                //obtenemos los pesos de su conglomerado
+                var PesosConglomerados = _servicioMantenimientos.Conglomerados.ConsultarPesosXConglomerado(idConglomerado);
+                // Obtenemos tipos de Objetivos
+                ViewData["ListaTiposObjetivos"] = _servicioMantenimientos.TiposObjetivos.ListarTiposObjetivos();
+                //Obtenemos tipos de competencias
+                ViewData["ListaTiposCompetencias"] = _servicioMantenimientos.TiposCompetencias.ListarTiposCompetencias();
+                //Obtenemos la lista de conglomerados
+                ViewData["ListaConglomerados"] = _servicioMantenimientos.Conglomerados.ListarConglomerados();
+                //obtenemos los objetivos y competencias relacionadas a este congloemrado
+                var (listaObjetivos, listaCompetencias) = _servicioMantenimientos.Evaluaciones.ListarObjYCompetenciasXConglomerado(idConglomerado);
+
+                //Obtenemos las competencias, comportamientos y descrp Transversales id 2500
+                var transversales = _servicioMantenimientos.ObtenerComportamientosYDescripciones.ListarComportamientosYDescripcionesNegocios(2500, "PorTipo");
+
+                var CompetenciasDelConglomerado = _servicioMantenimientos.ObtenerComportamientosYDescripciones.ListarComportamientosYDescripcionesNegociosXCOnglo(idConglomerado);
+                var tiposdeObjetivos = _servicioMantenimientos.TiposObjetivos.ListarTiposObjetivos();
+                //pasamos todo a la vista
+                ViewBag.ListaObjetivos = listaObjetivos;
+                ViewBag.ListaCompetencias = listaCompetencias;
+                ViewBag.PesosConglomerados = PesosConglomerados;
+                ViewBag.IdConglomerado = idConglomerado;
+                ViewBag.transversales = transversales;
+                ViewBag.CompetenciasDelConglomerado = CompetenciasDelConglomerado;
+                ViewBag.tiposdeObjetivos = tiposdeObjetivos;
+
+
+                return View(subalterno);
+
+            }
+            catch (Exception)
+            {
+                TempData["MensajeError"] = "Error al obtener las competencias.";
+                return View("Error");
+            }
+        }
 
         #endregion
 
